ca556e96953f28e0406f8d5849d3bfee2da54191
johnsauliNorima <john.sauli@Norimaconsulting,com>
2022-04-29 17:43:26 +0800

    first commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Mainmethods inventory= new Mainmethods();
+        inventory.Findfile();
+        inventory.Choices();
+    }
+}
